dfe0baa63699e66010ea9ee7b3aac49fe9b3498d
Raimo Radczewski <raimo@crftd.tech>
2026-05-13 23:35:49 +0200

    Replace heredoc with script

---
+++ _common/00_validate_signing_key.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+
+set -uexo pipefail
+
+PUBKEY_FROM_PRIVATE_KEY=$(openssl pkey -pubout -in /var/lib/dkms/mok.key)
+PUBKEY_FROM_CERTIFICATE=$(openssl x509 -noout -pubkey -in /var/lib/dkms/mok.pub)
+if [ "$PUBKEY_FROM_CERTIFICATE" != "$PUBKEY_FROM_PRIVATE_KEY" ] || [ -z "$PUBKEY_FROM_CERTIFICATE" ] || [ -z "$PUBKEY_FROM_PRIVATE_KEY" ];
+then
+  echo "Pubkeys don't match"
+  exit 1
+fi
