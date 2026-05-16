be58a57d39cc98a183c82cca9a7af00f23831f51
Raimo Radczewski <raimo@crftd.tech>
2026-05-16 11:37:28 +0200

    Pin x86_64 variant to avoid renovate issues with pinning the digest

--- ibp14/render.sh
+++ ibp14/render.sh
@@ -6,7 +6,7 @@ BASE_DIR="$( cd "$( dirname "$(realpath "$BASH_SOURCE")" )/../" && pwd )"
 source "$BASE_DIR/_tooling/render_snippets.sh"
 source "$BASE_DIR/_tooling/build_image_version.sh"
 
-BASE_IMAGE=quay.io/fedora/fedora-silverblue:44@sha256:b5d53351f73bfeada91bf1bd3af292ef9a7b219f283b8e4d8964009001e4080a
+BASE_IMAGE=quay.io/fedora/fedora-silverblue:44-x86_64@sha256:b5d53351f73bfeada91bf1bd3af292ef9a7b219f283b8e4d8964009001e4080a
 VERSION=$(build_image_version "$BASE_IMAGE")
 
 render_hat() {
--- simple-xfce/render.sh
+++ simple-xfce/render.sh
@@ -6,7 +6,7 @@ BASE_DIR="$( cd "$( dirname "$(realpath "$BASH_SOURCE")" )/../" && pwd )"
 source "$BASE_DIR/_tooling/render_snippets.sh"
 source "$BASE_DIR/_tooling/build_image_version.sh"
 
-BASE_IMAGE=quay.io/fedora/fedora-bootc:44@sha256:7e1dca740aafac268c5db352b40c5574804326b206c3769f5e77314457912a84
+BASE_IMAGE=quay.io/fedora/fedora-bootc:44-x86_64@sha256:7e1dca740aafac268c5db352b40c5574804326b206c3769f5e77314457912a84
 VERSION=$(build_image_version "$BASE_IMAGE")
 
 render_hat() {
