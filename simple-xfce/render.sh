51f2e04f4bdb805deb5f8942b7ca4669b83a41bb
Raimo Radczewski <raimo@crftd.tech>
2026-05-27 10:29:49 +0200

    Update digest

--- simple-xfce/render.sh
+++ simple-xfce/render.sh
@@ -6,7 +6,7 @@ BASE_DIR="$( cd "$( dirname "$(realpath "$BASH_SOURCE")" )/../" && pwd )"
 source "$BASE_DIR/_tooling/render_snippets.sh"
 source "$BASE_DIR/_tooling/build_image_version.sh"
 
-BASE_IMAGE=quay.io/fedora/fedora-bootc:44-x86_64@sha256:ba2b7cb0fde5a37bacb86d2ecd9f7ce7ad9f1564f162e68fb44cff20595070b8
+BASE_IMAGE=quay.io/fedora/fedora-bootc:44-x86_64@sha256:d5881f10505e72320afab95994ca092e334d4d31f47159d132662754bc26de9c
 VERSION=$(build_image_version "$BASE_IMAGE")
 
 render_hat() {
